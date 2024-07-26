9bfcb919c66b5bd92a6027296bd12297e02ef2ed
hydroperfox <hydroperfox@gmail.com>
2024-07-26 15:00:56 -0300

    Remove VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,12 @@
 
 use core::panic::PanicInfo;
 
-mod vga_buffer;
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
     loop {}
 }
 
-/// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
     loop {}
 }
