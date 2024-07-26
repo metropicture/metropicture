10278af44f66a7c91da302c79da35d9e709b80a0
hydroperfox <hydroperfox@gmail.com>
2024-07-26 11:36:59 -0300

    Update

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,9 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 bootloader_api::entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
